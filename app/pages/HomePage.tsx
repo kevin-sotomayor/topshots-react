f63d72bd1db245489c0fb9559bcb8d50eb0262d5
La-li-lu-le-lo <kevin.sotomayor@outlook.fr>
2025-04-17 15:25:15 +0200

    plane mesh implemented correctly

--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { useEffect } from "react";
 import "../styles/homepage.css";
+import posters from "../../data/projects.json";
 
 
 export function HomePage() {
@@ -8,30 +9,33 @@ export function HomePage() {
 		const appMain = document.querySelector(".app-homepage");
 		if (appMain) {
 			const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-			
+			const camera = new THREE.PerspectiveCamera(50, appMain.clientWidth / appMain.clientHeight, 0.1, 1000);
 			const renderer = new THREE.WebGLRenderer();
 			renderer.setSize(appMain.clientWidth, appMain.clientHeight);
-			renderer.setAnimationLoop( animate );
-			appMain?.appendChild( renderer.domElement );
-			
-			const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			const cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
-			
+			appMain.appendChild(renderer.domElement);
+
+			const planeMesh = new THREE.Mesh(
+				new THREE.PlaneGeometry(2, 2),
+				new THREE.MeshBasicMaterial({ color: 0xff0000 })
+			);
+			scene.add(planeMesh);
+
 			camera.position.z = 5;
-			
-			function animate() {
-			
-			  cube.rotation.x += 0.01;
-			  cube.rotation.y += 0.01;
-			
-			  renderer.render( scene, camera );
-			
-			}
+
+			const animate = () => {
+				requestAnimationFrame(animate);
+				renderer.render(scene, camera);
+			};
+			animate();
+
+			return () => {
+				renderer.dispose();
+				appMain.removeChild(renderer.domElement);
+			};
 		}
 	}, []);
+
+	console.log(posters);
 
 	return (
 		<main className="app-homepage">
