94fc02ec23d6c47473589d229ea88830adfe1e8f
La-li-lu-le-lo <kevin.sotomayor@outlook.fr>
2025-04-18 17:46:00 +0200

    working on component with r3f

--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -8,25 +8,40 @@ import "../styles/homepage.css";
 import data from "../../data/images.json";
 
 
-function Scene({}) {
+
+function PhotoElement() {
 	const texture = useLoader(THREE.TextureLoader, "/assets/images/amerique_du_sud_temple.jpg");
+	const [photoDimensions, setPhotoDimensions] = useState<[number, number]>([0, 0]);
+
+	useEffect(() => {
+		const canvas = document.querySelector(".app-homepage__canvas");
+        const updateDimensions = () => {
+			const canvasWidth = canvas!.clientWidth
+            const canvasHeight = canvas!.clientHeight;
+            const photoHeight = canvasHeight / 4;
+			const photoWidth = (16 / 9) * photoHeight;
+            setPhotoDimensions([photoWidth, photoHeight]);
+        };
+
+        updateDimensions();
+        window.addEventListener("resize", updateDimensions);
+        return () => window.removeEventListener("resize", updateDimensions);
+    }, []);
+
 	return (
-		<>
-			<ambientLight intensity={1.5} color={"white"}/>
-			<mesh>
-				<planeGeometry args={[16, 9]}/>
-				<meshStandardMaterial map={texture} />
-			</mesh>
-		</>
-	)
+		<mesh position={new THREE.Vector3(0, 0, 0)}>
+			<planeGeometry args={[16, 9]} />
+			<meshStandardMaterial map={texture} />
+		</mesh>
+	);
 }
 
 export function HomePage() {
 	return (
 		<main className="app-homepage">
-			<Canvas>
-				<Scene />
-				{/* <OrbitControls /> */}
+			<Canvas camera={ {fov: 50, near: 0.1, far: 1000, position: [0, 0, 25]} } className="app-homepage__canvas">
+				<ambientLight intensity={1.5} />
+				<PhotoElement />
 			</Canvas>
 		</main>
 	)
