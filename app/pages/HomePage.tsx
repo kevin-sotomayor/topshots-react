44f0fdf95212e9729c8f38b6ea5b99193ba7afc7
La-li-lu-le-lo <kevin.sotomayor@outlook.fr>
2025-04-18 14:23:57 +0200

    migrated to react three fiber due to performance issues

--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -1,120 +1,41 @@
+import { useState, useEffect, useRef, } from "react";
 import * as THREE from "three";
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { useState, useEffect, } from "react";
+import { Canvas, useFrame, } from "@react-three/fiber";
+import type { ThreeElements } from "@react-three/fiber";
+
 import "../styles/homepage.css";
 import data from "../../data/images.json";
-import { color } from "three/tsl";
 
 
 export function HomePage() {
-	useEffect(() => {
-		const appMain = document.querySelector(".app-homepage");
-		if (appMain) {
-			// const scene = new THREE.Scene();
-			// scene.background = new THREE.Color("#FFFFFF");
-			// const camera = new THREE.PerspectiveCamera(50, appMain.clientWidth / appMain.clientHeight, 0.1, 1000);
-			// const renderer = new THREE.WebGLRenderer();
-			// renderer.setSize(appMain.clientWidth, appMain.clientHeight);
-			// appMain.appendChild(renderer.domElement);
-			// camera.position.z = 25;
-			// const textureLoader = new THREE.TextureLoader();
+	function Box(props: ThreeElements['mesh']) {
+		const meshRef = useRef<THREE.Mesh>(null!)
+		const [hovered, setHover] = useState(false)
+		const [active, setActive] = useState(false)
+		useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+		return (
+			<mesh
+				{...props}
+				ref={meshRef}
+				scale={active ? 1.5 : 1}
+				onClick={(event) => setActive(!active)}
+				onPointerOver={(event) => setHover(true)}
+				onPointerOut={(event) => setHover(false)}>
+				<boxGeometry args={[1, 1, 1]} />
+				<meshStandardMaterial color={hovered ? 'hotpink' : '#2f74c0'} />
+			</mesh>
+		)
+	}
 
-			// // const meshArray = [];
-			// // for (let i = 0; i < data.images.length; i++) {
-				
-			// // }
-
-
-			// const texture = textureLoader.load("/assets/images/amerique_du_sud_temple.jpg")
-
-			// const planeMesh = new THREE.Mesh(
-			// 	new THREE.PlaneGeometry(46, 24),
-			// 	new THREE.MeshBasicMaterial({ map: texture })
-			// );
-
-			// planeMesh.position.x = appMain.clientWidth;
-			// planeMesh.position.y = appMain.clientHeight;
-
-			// console.log(appMain.clientWidth / 25);
-
-			// planeMesh.position.x = 0;
-			// planeMesh.position.y = 0;
-			// scene.add(planeMesh);
-
-
-			// const animate = () => {
-			// 	requestAnimationFrame(animate);
-			// 	renderer.render(scene, camera);
-			// };
-			// animate();
-
-			// return () => {
-			// 	renderer.dispose();
-			// 	appMain.removeChild(renderer.domElement);
-			// };
-
-			const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera(50, appMain.clientWidth / appMain.clientHeight, 0.1, 1000);
-			const textureLoader = new THREE.TextureLoader();
-	
-			camera.position.set(0, 0, 1);
-			camera.lookAt(0, 0, 0);
-			camera.aspect = appMain.clientWidth / appMain.clientHeight;
-	
-			const axesHelper = new THREE.AxesHelper(100);
-			scene.add(axesHelper);
-	
-			const renderer = new THREE.WebGLRenderer({
-				antialias: true,
-			});
-	
-			scene.background = new THREE.Color().setRGB(1., 1., 1.);
-	
-			renderer.setSize(appMain.clientWidth, appMain.clientHeight);
-			renderer.setPixelRatio(window.devicePixelRatio);
-			renderer.setAnimationLoop(renderLoop);
-	
-			const canvas = renderer.domElement;
-			appMain.appendChild(canvas);
-	
-			camera.position.z = 5;
-	
-			// const controls = new OrbitControls(camera, canvas);
-			// controls.enableDamping = false;
-			// controls.autoRotate = false;
-
-			const meshArray = [];
-			for (let i = 0; i < data.images.length; i++) {
-
-			}
-
-	
-			window.addEventListener("resize", () => {
-				camera.aspect = appMain.clientWidth / appMain.clientHeight;
-				camera.updateProjectionMatrix();
-				renderer.setSize(appMain.clientWidth, appMain.clientHeight);
-				renderer.setPixelRatio(window.devicePixelRatio);
-			});
-	
-			function renderLoop() {
-				// controls.update();
-				renderer.render(scene, camera);
-				window.requestAnimationFrame(renderLoop);
-				camera.updateProjectionMatrix();
-			}
-
-			return () => {
-				renderer.dispose();
-				appMain.removeChild(renderer.domElement);
-			};
-		}
-
-
-	}, []);
-
-	// console.log(posters);
 	return (
 		<main className="app-homepage">
+			<Canvas>
+				<ambientLight intensity={Math.PI / 2} />
+				<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+				<pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+				<Box position={[-1.2, 0, 0]} />
+				<Box position={[1.2, 0, 0]} />
+			</Canvas>,
 			{/* {
 				photos && (
 					photos.map((image, index) => (
--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,6 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 
 export default [
 	index("./routes/home.tsx"),
+	// route("/projects", "/routes/projects.tsx"),
+	// route("/team", "/routes/team.tsx"),
 ] satisfies RouteConfig;
