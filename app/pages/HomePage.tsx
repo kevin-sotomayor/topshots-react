c6379cea2d41e06c96cfddb36e79838fd776c8bb
La-li-lu-le-lo <kevin.sotomayor@outlook.fr>
2025-04-23 17:19:29 +0200

    now importing video

--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -1,125 +1,46 @@
-import { useState, useEffect, useRef, } from "react";
+import type { ThreeElements, } from "@react-three/fiber";
+
+import { useState, useEffect, useRef, Suspense} from "react";
 import * as THREE from "three";
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import type { ThreeElements } from "@react-three/fiber";
-import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame, useLoader, extend, } from "@react-three/fiber";
+import { PerspectiveCamera, OrbitControls, useVideoTexture, Plane, useTexture, SoftShadows} from "@react-three/drei";
 
 import "../styles/homepage.css";
-import data from "../../data/images.json";
+import video from "../../assets/video.mp4";
+import img from "../../assets/images/amerique_du_sud_temple.jpg";
 
 
-function PhotoElement() {
-	const [photosData, setPhotosData] = useState<[] | string[]>([]);
-	const [photoDimensions, setPhotoDimensions] = useState<[number, number]>([0, 0]);
-	const [canvasFov, setCanvasFov] = useState<number[]>([0, 0]);
-	const [firstRowPosition, setfirstRowPosition] = useState<| number>(0);
-	const [vertexShader, setVertexShader] = useState<string>("");
-	const [fragmentShader, setFragmentShader] = useState<string>("");
+function VideosShaderMaterial() {
+	
+}
 
-	const cameraPosition = 5; // It is the default value
 
-	function loadTexture(url: string) {
-		const texture = useLoader(THREE.TextureLoader, url);
-		return texture;
-	}
-
-	useEffect(() => {
-		if (!data) return;
-		setPhotosData(data);
-
-        const updateDimensions = () => {
-			const canvas = document.querySelector(".app-homepage__canvas");
-			if (!canvas) return;
-			
-			const canvasWidth = canvas.clientWidth;
-            const canvasHeight = canvas.clientHeight;
-
-			const fov = 50;
-			const fovInRadians = (fov * Math.PI) / 180;
-
-			const visibleHeight = 2 * Math.tan(fovInRadians / 2) * cameraPosition;
-			const aspectRatio = canvasWidth / canvasHeight;
-			const visibleWidth = visibleHeight * aspectRatio;
-			setCanvasFov([visibleWidth, visibleHeight]);
-			console.log("canvas fov : ", canvasFov);
-
-			const photoHeight = visibleHeight / 4;
-			const photoWidth = (16 / 9) * photoHeight;
-			setPhotoDimensions([photoWidth, photoHeight]);
-
-			console.log("photo dimesions : ", photoDimensions);
-
-			// const firstRowX = -visibleWidth / 2 + photoWidth / 2;
-			const firstRowY = visibleHeight / 2 - photoHeight / 2;
-			setfirstRowPosition(firstRowY);
-        };
-
-		const vertexShader = `
-			varying vec2 vUv;
-
-  			void main() {
-				vUv = uv;
-				gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-  			}
-		`;
-		
-		const fragmentShader = `
-			varying vec2 vUv;
-			// uniform float uHover;
-			uniform sampler2D uTexture;
-
-			void main() {
-				vec4 textureColor = texture2D(uTexture, vUv);
-				// vec3 alteredColor = mix(textureColor.rgb, textureColor.rgb * vec3(0., 0., 1.), uHover);
-				vec3 alteredColor = mix(textureColor.rgb, textureColor.rgb * vec3(0., 0., 1), 0.25);
-				gl_FragColor = vec4(alteredColor, textureColor.a); 
-			}
-		`;
-
-		setVertexShader(vertexShader);
-		setFragmentShader(fragmentShader);
-
-        updateDimensions();
-        window.addEventListener("resize", updateDimensions);
-        return () => window.removeEventListener("resize", updateDimensions);
-    }, [cameraPosition]);
-
+function VideoMaterial() {
+	const texture = useVideoTexture(video, {autoplay: true, start: true, loop: true});
 	return (
-		<group position={[0, firstRowPosition, 0]}>
-			{photosData && photosData.map((imageUrl, index) => {
-				const texture = loadTexture(imageUrl);
-
-				return (
-					<mesh 
-						key={index} 
-						position={[-canvasFov[0] / 2 + photoDimensions[0] / 2 + photoDimensions[0] * index, 0,0]} 
-					>
-						<planeGeometry args={photoDimensions} />
-						<shaderMaterial
-							vertexShader={vertexShader} 
-							fragmentShader={fragmentShader} 
-							uniforms={{ 
-								uTexture: { value: texture }, 
-							}}
-							transparent = {true}
-						/>
-					</mesh>
-				)
-			})}
-		</group>
+		<meshBasicMaterial side={THREE.DoubleSide} map={texture} />
 	)
 }
+
+function VideoElement() {
+	return (
+		<Canvas className="app-homepage__canvas" camera={{ position: [0, 0, 7.5]}}>
+			<Plane args={[16, 9]}>
+				<VideoMaterial />
+			</Plane>
+			<ambientLight color={new THREE.Color(0xff0000)} intensity={2}/>
+			<OrbitControls />
+			<SoftShadows />
+		</Canvas>
+	)
+}
+
 
 export function HomePage() {
 	return (
 		<main className="app-homepage">
-			<Canvas 
-				className="app-homepage__canvas"
-				camera={{ fov: 50, near: 0.1, far: 1000, position: [0, 0, 5] }}
-			>
-				<ambientLight intensity={2} />
-				<PhotoElement />
-			</Canvas>
+			{/* <video src={video} autoPlay={true} loop={true} controls={true}/> */}
+			<VideoElement />
 		</main>
 	)
 }
